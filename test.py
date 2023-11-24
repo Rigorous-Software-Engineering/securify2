863d844e5ff428370c706f4a2b807e5944dde538
Robin Knoll <e11828955@student.tuwien.ac.at>
2023-11-24 13:12:02 +0100

    worked on getting AST

--- test.py
+++ test.py
@@ -1,29 +1,37 @@
+import json
 from securify.solidity import solidity_ast_compiler, solidity_cfg_compiler
+from securify.staticanalysis.static_analysis import StaticAnalysisResult
 from securify.staticanalysis.visualization import visualize
 from securify.staticanalysis.factencoder import encode
 from securify.analyses.analysis import AnalysisConfiguration, AnalysisContext
+from sys import argv
 
-# ast = solidity_ast_compiler.compile_ast("./test.sol")
+if len(argv) < 2:
+    print("Missing argument: contract path")
+    exit()
 
-# cfg = solidity_cfg_compiler.compile_cfg(ast)
+contract = argv[1]
+contract_name = contract.replace(".sol", "")
 
 config = AnalysisConfiguration(
         # TODO: this returns only the dict ast, but should return the object representation
         ast_compiler=lambda t: solidity_ast_compiler.compile_ast(t.source_file),
         cfg_compiler=lambda t: solidity_cfg_compiler.compile_cfg(t.ast).cfg,
-        static_analysis=lambda t: None
+        static_analysis=lambda t: StaticAnalysisResult([], {}, {}, "", "", 0)
     )
 
 context = AnalysisContext(
         config=config,
-        source_file="./test.sol"
+        source_file=contract
     )
-# print(cfg)
 
 ast = context.ast
-print(ast)
+
+with open(contract_name + ".ast", "w") as outfile:
+    version = ast["_solc_version"]# getattr(ast, "_solc_version")
+    ast["_solc_version"] = str(version)
+    json.dump(ast, outfile)
 
 facts, _ = encode(context.cfg)
 print(context.cfg)
-print(facts)
 visualize(facts).render("out/dl", format="svg", cleanup=True)
--- test.sol
+++ test.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.8.19;
 
 contract HelloWorld {
-    function my_function() private pure returns (int res) {
-        int my_variable = 5;
+    function my_simple_function() private pure returns (int res) {
+        string memory hello = "hello world";
+
+        return 5;
+    }
+
+    function my_middle_function() private pure returns (int res) {
+        int my_variable = my_simple_function();
         return my_variable;
     }
 
     function a_complex_function() public returns (int res) {
-        int result = my_function();
+        int result = my_middle_function();
 
         return result;
     }
